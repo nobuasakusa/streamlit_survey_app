fcd4c7adf5d5a5ae7f33d4cb18e1eb94e541e862
NobuAsakusa <113888063+nobuasakusa@users.noreply.github.com>
2024-02-01 00:54:00 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+import streamlit as st
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+
+# Streamlitアプリのタイトル
+st.title('ワードクラウド生成器')
+
+# ユーザー入力を受け取る
+user_input = st.text_area("ここにテキストを入力してください", "")
+
+# ワードクラウドを生成する関数
+def generate_wordcloud(text):
+    wordcloud = WordCloud(
+        font_path='./NotoSansJP-Regular.otf',  # Google Fontsの日本語フォントのパス
+        width=800,
+        height=800,
+        background_color='white',
+        min_font_size=10
+    ).generate(text)
+    return wordcloud
+
+# ユーザー入力があればワードクラウドを表示
+if user_input:
+    wordcloud = generate_wordcloud(user_input)
+    plt.figure(figsize=(8, 8), facecolor=None)
+    plt.imshow(wordcloud)
+    plt.axis("off")
+    plt.tight_layout(pad=0)
+
+    st.pyplot(plt)
